c0bea4ecf8b367c59c253302ebac9ba5c8c29117
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-08 08:28:06 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,9 +7,6 @@ class ChattyStudent < Student
   end
   
   def raise_hand
-    10.times do 
-      super
-    end
-    puts "Pick me!"
+    10.times do {|super| super puts "Pick me!"}
   end
-end
+    
